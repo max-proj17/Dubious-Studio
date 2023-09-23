4a2ad2a5b581fb5c304979cbfa044195b5f274cd
Sam Witte <sam@samwitte.com>
2023-09-23 10:58:25 -0500

    Updated canvas to have a few preset colors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene
-from PyQt6.QtGui import QPainter, QPen
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QVBoxLayout, QWidget, QPushButton, QButtonGroup, QDockWidget
+from PyQt6.QtGui import QPainter, QPen, QColor
 from PyQt6.QtCore import Qt, QPoint
 
 class DrawingCanvas(QGraphicsView):
@@ -9,6 +9,14 @@ class DrawingCanvas(QGraphicsView):
         self.startPoint = QPoint()
         self.endPoint = QPoint()
         self.isDrawing = False
+        self.currentColor = QColor(Qt.GlobalColor.black)
+        self.currentTool = "draw"
+
+    def setTool(self, tool):
+        self.currentTool = tool
+
+    def setColor(self, color):
+        self.currentColor = QColor(color)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
@@ -19,7 +27,11 @@ class DrawingCanvas(QGraphicsView):
     def mouseMoveEvent(self, event):
         if self.isDrawing:
             self.endPoint = event.position().toPoint()
-            self.scene().addLine(self.startPoint.x(), self.startPoint.y(), self.endPoint.x(), self.endPoint.y())
+            pen = QPen(self.currentColor)
+            if self.currentTool == "erase":
+                pen.setColor(Qt.GlobalColor.white)
+                pen.setWidth(10)
+            self.scene().addLine(self.startPoint.x(), self.startPoint.y(), self.endPoint.x(), self.endPoint.y(), pen)
             self.startPoint = self.endPoint
 
     def mouseReleaseEvent(self, event):
@@ -32,14 +44,44 @@ class DrawingApp(QMainWindow):
 
         # Create a white QGraphicsScene with 500x500 dimensions
         self.scene = QGraphicsScene(self)
-        self.scene.setSceneRect(0, 0, 800, 800)
+        self.scene.setSceneRect(0, 0, 500, 500)
         self.scene.setBackgroundBrush(Qt.GlobalColor.white)
 
         # Create the custom QGraphicsView for drawing
         self.canvas = DrawingCanvas(self.scene, self)
         self.setCentralWidget(self.canvas)
 
-        self.setGeometry(100, 100, 820, 820)
+        # Sidebar with color options and tools
+        self.sidebar = QWidget()
+        self.layout = QVBoxLayout(self.sidebar)
+
+        self.colors = ["black", "red", "green", "blue", "yellow"]
+        self.colorButtons = QButtonGroup(self.sidebar)
+        for color in self.colors:
+            btn = QPushButton(color.capitalize())
+            btn.setStyleSheet(f"background-color: {color}")
+            btn.setCheckable(True)
+            self.colorButtons.addButton(btn)
+            self.layout.addWidget(btn)
+            btn.clicked.connect(lambda checked, color=color: self.canvas.setColor(color))
+
+        self.tools = ["draw", "erase"]
+        self.toolButtons = QButtonGroup(self.sidebar)
+        for tool in self.tools:
+            btn = QPushButton(tool.capitalize())
+            btn.setCheckable(True)
+            self.toolButtons.addButton(btn)
+            self.layout.addWidget(btn)
+            btn.clicked.connect(lambda checked, tool=tool: self.canvas.setTool(tool))
+
+        self.colorButtons.buttons()[0].setChecked(True)
+        self.toolButtons.buttons()[0].setChecked(True)
+
+        self.dock = QDockWidget("Tools", self)
+        self.dock.setWidget(self.sidebar)
+        self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dock)
+
+        self.setGeometry(100, 100, 620, 520)
         self.setWindowTitle('Drawing Canvas')
         self.show()
 
