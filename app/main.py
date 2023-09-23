be4d134e1f93fb3b23b81acb40993e6bd3e3f3fb
Sam Witte <sam@samwitte.com>
2023-09-23 10:56:07 -0500

    Switched to PyQt6.

--- app/main.py
+++ app/main.py
@@ -1,20 +1,49 @@
-import dearpygui.dearpygui as dpg
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene
+from PyQt6.QtGui import QPainter, QPen
+from PyQt6.QtCore import Qt, QPoint
 
-dpg.create_context()
-dpg.create_viewport(title='Dubious Studio', width=800, height=800)
+class DrawingCanvas(QGraphicsView):
+    def __init__(self, scene, parent=None):
+        super(DrawingCanvas, self).__init__(scene, parent)
+        self.startPoint = QPoint()
+        self.endPoint = QPoint()
+        self.isDrawing = False
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello, world")
-    dpg.add_button(label="Save")
-    dpg.add_input_text(label="string", default_value="Quick brown fox")
-    dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
-    
-    # Create a drawing canvas
-    with dpg.drawlist(width=500, height=500) as drawlist:
-        # Example: Draw a red line on the canvas
-        dpg.draw_rectangle((0, 0), (500, 500), color=(255, 255, 255, 255), fill=(255, 255, 255, 255))
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.startPoint = event.position().toPoint()
+            self.endPoint = self.startPoint
+            self.isDrawing = True
 
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+    def mouseMoveEvent(self, event):
+        if self.isDrawing:
+            self.endPoint = event.position().toPoint()
+            self.scene().addLine(self.startPoint.x(), self.startPoint.y(), self.endPoint.x(), self.endPoint.y())
+            self.startPoint = self.endPoint
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.isDrawing = False
+
+class DrawingApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Create a white QGraphicsScene with 500x500 dimensions
+        self.scene = QGraphicsScene(self)
+        self.scene.setSceneRect(0, 0, 800, 800)
+        self.scene.setBackgroundBrush(Qt.GlobalColor.white)
+
+        # Create the custom QGraphicsView for drawing
+        self.canvas = DrawingCanvas(self.scene, self)
+        self.setCentralWidget(self.canvas)
+
+        self.setGeometry(100, 100, 820, 820)
+        self.setWindowTitle('Drawing Canvas')
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = DrawingApp()
+    sys.exit(app.exec())
