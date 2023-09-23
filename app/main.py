448cf3e5a7119787478b6d5c79c2b7fbc242a9c6
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-09-23 12:20:32 -0500

    Panning, Zooming, Rotating and dynamic canvas added!

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QVBoxLayout, QWidget, QPushButton, QButtonGroup, QDockWidget, QColorDialog, QListWidget, QListWidgetItem
-from PyQt6.QtGui import QPainter, QPen, QColor
-from PyQt6.QtCore import Qt, QPoint, QSize
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGraphicsView, QGraphicsScene, QVBoxLayout, QWidget, QPushButton, QButtonGroup, QDockWidget, QColorDialog, QListWidget, QListWidgetItem, QGraphicsRectItem
+from PyQt6.QtGui import QPainter, QPen, QColor, QTransform, QBrush
+from PyQt6.QtCore import Qt, QPoint, QSize, QRectF
 
 
 class ColorPalette(QWidget):
@@ -31,6 +31,7 @@ class ColorPalette(QWidget):
             return item.background().color()
         return None
 
+
 class DrawingCanvas(QGraphicsView):
     def __init__(self, scene, parent=None):
         super(DrawingCanvas, self).__init__(scene, parent)
@@ -40,22 +41,49 @@ class DrawingCanvas(QGraphicsView):
         self.currentColor = QColor(Qt.GlobalColor.black)
         self.currentTool = "draw"
 
+        self.scaleFactor = 1.0
+        self.rotationAngle = 0.0
+        self.lastMousePosition = QPoint()
+
+        self.setBackgroundBrush(QBrush(QColor(169, 169, 169)))  # Dark Grey Background
+
+        # Create a white canvas item
+        self.canvasItem = QGraphicsRectItem(0, 0, 1024, 1024)
+        self.canvasItem.setBrush(QBrush(Qt.GlobalColor.white))
+
+        # Set the scene rect to show the dark grey background around the canvas
+        self.setSceneRect(-1024, -1024, 2048, 2048)
+
+        # Center the canvas item in the scene
+        self.canvasItem.setPos(-self.canvasItem.rect().width() / 2, -self.canvasItem.rect().height() / 2)
+        self.scene().addItem(self.canvasItem)
+
     def setTool(self, tool):
         self.currentTool = tool
 
     def setColor(self, color):
         self.currentColor = QColor(color)
 
+    def wheelEvent(self, event):
+        if event.modifiers() == Qt.KeyboardModifier.ControlModifier:
+            delta = event.angleDelta().y()
+            factor = 1.1 if delta > 0 else 0.9
+            self.scaleFactor *= factor
+            self.applyTransform()
+
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
-            # self.startPoint = event.position().toPoint()
-            self.startPoint = self.mapToScene((event.position().toPoint()))
+            self.startPoint = self.mapToScene(event.position().toPoint())
             self.endPoint = self.startPoint
             self.isDrawing = True
+        elif event.button() == Qt.MouseButton.RightButton:
+            self.lastMousePosition = event.globalPosition().toPoint()
+            self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)  # Enable scrolling drag mode
+            self.setCursor(Qt.CursorShape.ClosedHandCursor)  # Change cursor to closed hand
+            event.accept()
 
     def mouseMoveEvent(self, event):
         if self.isDrawing:
-            # self.endPoint = event.position().toPoint()
             self.endPoint = self.mapToScene(event.position().toPoint())
             pen = QPen(self.currentColor)
             if self.currentTool == "erase":
@@ -63,25 +91,44 @@ class DrawingCanvas(QGraphicsView):
                 pen.setWidth(10)
             self.scene().addLine(self.startPoint.x(), self.startPoint.y(), self.endPoint.x(), self.endPoint.y(), pen)
             self.startPoint = self.endPoint
+        elif event.buttons() & Qt.MouseButton.RightButton:
+            delta = event.globalPosition().toPoint() - self.lastMousePosition
+            if event.modifiers() == Qt.KeyboardModifier.ShiftModifier:
+                self.rotationAngle += delta.x() * 0.5
+            else:
+                self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() - delta.x())
+                self.verticalScrollBar().setValue(self.verticalScrollBar().value() - delta.y())
+            self.lastMousePosition = event.globalPosition().toPoint()
+            self.applyTransform()
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
             self.isDrawing = False
+        elif event.button() == Qt.MouseButton.RightButton:
+            self.setDragMode(QGraphicsView.DragMode.NoDrag)  # Disable scrolling drag mode
+            self.setCursor(Qt.CursorShape.ArrowCursor)  # Change cursor back to arrow
+            event.accept()
+
+    def applyTransform(self):
+        # Reset the view transformation
+        self.resetTransform()
+        # Apply transformations to the canvas item
+        transform = QTransform()
+        transform.scale(self.scaleFactor, self.scaleFactor)
+        transform.rotate(self.rotationAngle)
+        self.canvasItem.setTransform(transform)
+
 
 class DrawingApp(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Create a white QGraphicsScene with 500x500 dimensions
         self.scene = QGraphicsScene(self)
-        self.scene.setSceneRect(0, 0, 500, 500)
-        self.scene.setBackgroundBrush(Qt.GlobalColor.white)
+        self.scene.setSceneRect(-512, -512, 2048, 2048)  # Extend scene size to show dark grey background
 
-        # Create the custom QGraphicsView for drawing
         self.canvas = DrawingCanvas(self.scene, self)
         self.setCentralWidget(self.canvas)
 
-        # Sidebar with color options and tools
         self.sidebar = QWidget()
         self.layout = QVBoxLayout(self.sidebar)
 
@@ -99,17 +146,21 @@ class DrawingApp(QMainWindow):
 
         self.toolButtons.buttons()[0].setChecked(True)
 
-        self.colorPalette.colorList.itemClicked.connect(lambda: self.canvas.setColor(self.colorPalette.getSelectedColor()))
+        self.colorPalette.colorList.itemClicked.connect(
+            lambda: self.canvas.setColor(self.colorPalette.getSelectedColor()))
 
         self.dock = QDockWidget("Tools", self)
         self.dock.setWidget(self.sidebar)
         self.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dock)
 
-        self.setGeometry(100, 100, 620, 520)
+        self.setGeometry(100, 100, 1144, 1144)  # Adjust window size to accommodate the larger canvas
         self.setWindowTitle('Drawing Canvas')
         self.show()
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = DrawingApp()
     sys.exit(app.exec())
+
