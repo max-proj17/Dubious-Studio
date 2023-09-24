4483a57ae3399233fb8e08b6540d025dd1c8ee41
Maximelio-Finch <87384802+max-proj17@users.noreply.github.com>
2023-09-24 08:07:06 -0500

    I've successfully applied OOP to our GAITs

--- lib/AIWidget.py
+++ lib/AIWidget.py
@@ -5,35 +5,130 @@ import shutil
 import requests
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QLineEdit, QStackedWidget, QComboBox
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtCore import Qt, QDir
+from PyQt6.QtCore import Qt, QDir, QThread, pyqtSignal
 from dotenv import load_dotenv
 
-class SketchToImageWidget(QWidget):
-    def __init__(self):
+class ImageProcessingThread(QThread):
+    finished_signal = pyqtSignal(dict)  # Signal to emit the result of the processing
+    
+    def __init__(self, image_path, stablexl_key, prompt):
         super().__init__()
+        self.image_path = image_path
+        self.stablexl_key = stablexl_key
+        self.prompt = prompt
+        
+    def run(self):
+        # Perform the image processing here
+        result = {}
+        desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
+      
+        
+        url = "https://clipdrop-api.co/sketch-to-image/v1/sketch-to-image"
+        headers = {"x-api-key": self.stablexl_key}
+        
+        old_image_name = os.path.basename(self.image_path)
+        
+        with open(self.image_path, 'rb') as image_file:
+            files = {'sketch_file': (old_image_name, image_file, 'image/png')}
+            data = {'prompt': self.prompt}
+            response = requests.post(url, headers=headers, files=files, data=data)
+    
+        if response.status_code == 200:
+            timestamp = int(time.time())
+            new_image_name = f'new_image_{timestamp}.jpg'
+            new_image_path = os.path.join(desktop_path, new_image_name)
+            
+            with open(new_image_path, 'wb') as new_image_file:
+                new_image_file.write(response.content)
+            
+            result['new_image_path'] = new_image_path
+        else:
+            result['error'] = f"API Response: {response.status_code}, {response.text}"
+        
+        self.finished_signal.emit(result)
+        
+class SketchToImageWidget(QWidget):
+    def __init__(self, stablexl_key):
+        super().__init__()
+        self.stablexl_key = stablexl_key  # Store the API key
         # Initialize UI components for Sketch to Image tool
+        self.init_ui()
+        
+    def init_ui(self):
+        # Create layout
+        layout = QVBoxLayout()
 
+        # Create upload button
+        self.upload_btn = QPushButton('Upload Image')
+        self.upload_btn.clicked.connect(self.upload_image)
+        layout.addWidget(self.upload_btn)
+
+        # Create prompt input field
+        self.prompt_input = QLineEdit()
+        self.prompt_input.setPlaceholderText("Enter prompt describing the content to generate")
+        layout.addWidget(self.prompt_input)
+        
+        # Create process button
+        self.process_btn = QPushButton('Process Image')
+        self.process_btn.clicked.connect(self.process_image)
+        layout.addWidget(self.process_btn)
+
+        # Create image label
+        self.image_label = QLabel()
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.image_label)
+
+        # Set layout
+        self.setLayout(layout)
+        
+    def upload_image(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Open Image File', QDir.homePath(), 'Images (*.png)')
+        if file_name:
+            pixmap = QPixmap(file_name)
+            scaled_pixmap = pixmap.scaled(pixmap.width() // 4, pixmap.height() // 4, Qt.AspectRatioMode.KeepAspectRatio)
+            self.image_label.setPixmap(scaled_pixmap)
+            self.current_image_path = file_name
+            
+    def process_image(self):
+        print("Processing image...")
+        prompt = self.prompt_input.text().strip()
+        if hasattr(self, 'current_image_path') and self.stablexl_key:
+            self.processing_thread = ImageProcessingThread(self.current_image_path, self.stablexl_key, prompt)
+            self.processing_thread.finished_signal.connect(self.on_image_processed)
+            self.processing_thread.start()
+            
+    def on_image_processed(self, result):
+        if 'new_image_path' in result:
+            pixmap = QPixmap(result['new_image_path'])
+            scaled_pixmap = pixmap.scaled(pixmap.width() // 4, pixmap.height() // 4, Qt.AspectRatioMode.KeepAspectRatio)
+            self.image_label.setPixmap(scaled_pixmap)
+            self.current_image_path = result['new_image_path']
+        elif 'error' in result:
+            print(f"Error: Unable to process image. {result['error']}")
+        
 class TextToImageWidget(QWidget):
     def __init__(self):
         super().__init__()
         # Initialize UI components for Text to Image tool
 
+    # Implement the process_image method for Text to Image tool
+    def process_image(self):
+        pass  # Replace with actual implementation
+
 class ImageUpscalingWidget(QWidget):
     def __init__(self):
         super().__init__()
         # Initialize UI components for Image Upscaling tool
-        
-class SketchToImageWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        # Initialize UI components for sketch to image tool
+
+    # Implement the process_image method for Image Upscaling tool
+    def process_image(self):
+        pass  # Replace with actual implementation
+
         
 class AIWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        # Initialize UI components
-        self.init_ui()
         # Load environment variables from .env file
         load_dotenv()
 
@@ -41,7 +136,6 @@ class AIWidget(QWidget):
         self.stablexl_key = os.getenv("STABLEXL_API_KEY")
         self.openai_key = os.getenv("OPENAI_API_KEY")
         
-
         # Create layout
         layout = QVBoxLayout()
 
@@ -55,7 +149,7 @@ class AIWidget(QWidget):
         layout.addWidget(self.stackedWidget)
 
         # Add tool widgets to the stacked widget
-        self.stackedWidget.addWidget(SketchToImageWidget())
+        self.stackedWidget.addWidget(SketchToImageWidget(self.stablexl_key))  # Pass the API key to the widget
         self.stackedWidget.addWidget(TextToImageWidget())
         self.stackedWidget.addWidget(ImageUpscalingWidget())
 
@@ -64,68 +158,6 @@ class AIWidget(QWidget):
 
         # Set layout
         self.setLayout(layout)
-
-    def upload_image(self):
-        # Open file dialog to select image
-        file_name, _ = QFileDialog.getOpenFileName(self, 'Open Image File', QDir.homePath(), 'Images (*.png)')
-        if file_name:
-            # Display the selected image
-            pixmap = QPixmap(file_name)
-            scaled_pixmap = pixmap.scaled(pixmap.width() // 4, pixmap.height() // 4, Qt.AspectRatioMode.KeepAspectRatio)
-            self.image_label.setPixmap(scaled_pixmap)
-            self.current_image_path = file_name
-
-    def process_image(self):
-        print("Processing image...")
-        prompt = self.prompt_input.text().strip()
-        # Check if there is an old image and API key is available
-        if hasattr(self, 'current_image_path') and self.stablexl_key:
-            print("Image and API key are available...")
-            # Save old image to desktop
-            desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-            old_image_name = os.path.basename(self.current_image_path)
-            old_image_path = os.path.join(desktop_path, old_image_name)
-          
-            try:
-                print("About to enter shutil code")
-                shutil.copy(self.current_image_path, old_image_path)  # Copy the original image to the desktop
-                print( "Post shutil")
-            except Exception as e:
-                print("Exception occurred:", e)
-            # Define the URL, headers, and prompt
-            url = "https://clipdrop-api.co/sketch-to-image/v1/sketch-to-image"
-            headers = {
-                "x-api-key": self.stablexl_key,
-            }
-
-            # Make API call and process the image
-            with open(self.current_image_path, 'rb') as image_file:  # Use the original image file for the API call
-                files = {'sketch_file': (old_image_name, image_file, 'image/png')}
-                data = {'prompt': prompt}
-                response = requests.post(url, headers=headers, files=files, data=data)
-            
-            print("Called the API key")
-
-                
-            # Handle the API response
-        if response.status_code == 200:
-            # Save and display the new image
-            print("API call successful...")
-            
-            # Generate a unique filename using a timestamp
-            timestamp = int(time.time())
-            new_image_name = f'new_image_{timestamp}.jpg'
-            new_image_path = os.path.join(desktop_path, new_image_name)
-            
-            with open(new_image_path, 'wb') as new_image_file:
-                new_image_file.write(response.content)
-            pixmap = QPixmap(new_image_path)
-            scaled_pixmap = pixmap.scaled(pixmap.width() // 4, pixmap.height() // 4, Qt.AspectRatioMode.KeepAspectRatio)
-            self.image_label.setPixmap(scaled_pixmap)
-            self.current_image_path = new_image_path
-        else:
-            # Handle API errors
-            print(f"Error: Unable to process image. API Response: {response.status_code}, {response.text}")
 
 
 
